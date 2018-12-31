875af791445f2f1456127459e1400fb9112c98ae
Martyn Janes <martyn@obany.com>
2018-12-31 09:42:45 +0300

    Updated for new iota.js

--- src/index.js
+++ src/index.js
@@ -1,32 +1,19 @@
-const IOTA = require('iota.lib.js');
+const { asciiToTrytes, trytesToAscii } = require("@iota/converter");
 const Mam = require('mam.client.js');
 
-// Create IOTA instance directly with provider
-const iota = new IOTA({
-    provider: "https://nodes.iota.fm:443/"
-});
-
-// What is the IOTA lib version
-console.log("IOTA Version", iota.version);
-
-// If your node provider does not support attachToTangle you can uncomment the
-// following section to use WebGL proof of work
-// try {
-//     const curl = require('curl.lib.js');
-//     curl.init();
-//     curl.overrideAttachToTangle(iota);
-// } catch (err) {
-//     console.error("Error", err);
-// }
+const provider = "https://nodes.thetangle.org:443";
+const attachDepth = 3;
+const attachMwm = 14; // Use 9 for devnet
+const explorer = "https://thetangle.org" // use https://devnet.thetangle.org for devnet;
 
 // Initialise Mam, if you fork the sandbox you should change the seed
 console.log("Initializing Mam");
 // Create a 81 trytes seed if you want to show previous messages in your mam stream
-const seed = undefined; 
+const seed = undefined;
 if (!seed) {
     console.error("As you have not provided an 81 tryte seed you will never get more than one message in your mam stream");
 }
-let mamState = Mam.init(iota, seed);
+let mamState = Mam.init(provider, seed);
 const initialRoot = Mam.getRoot(mamState);
 
 // Set the channel mode
@@ -47,7 +34,7 @@ function publishMessage(mamState, trytesMessage) {
 
     // Attach the payload
     console.log("Attaching payload, please wait...")
-    return Mam.attach(message.payload, message.address)
+    return Mam.attach(message.payload, message.address, attachDepth, attachMwm)
         .then(() => message);
 }
 
@@ -58,7 +45,7 @@ function fetchMessages(messageRoot) {
     return Mam.fetch(messageRoot, channelMode, retrictedSideKeyTrytes)
         .then(response => {
             response.messages.forEach(messageTrytes => {
-                console.log("Fetched Message", iota.utils.fromTrytes(messageTrytes));
+                console.log("Fetched Message", trytesToAscii(messageTrytes));
             });
             console.log("Next Root", response.nextRoot);
             return response;
@@ -73,15 +60,18 @@ fetchMessages(Mam.getRoot(mamState))
     .then((messageResponse) => {
         mamState.channel.start = messageResponse.messages.length;
 
-        publishMessage(mamState, iota.utils.toTrytes(`This is my message ${messageResponse.messages.length + 1}`))
+        publishMessage(mamState, asciiToTrytes(`This is my message ${messageResponse.messages.length + 1}`))
             .then((message) => {
                 console.log("Message Published");
                 if (channelMode === "public") {
-                    console.log("You can view the message chain on the tangle", `https://thetangle.org/mam/${initialRoot}`);
-                    console.log("or just for this message at", `https://thetangle.org/mam/${message.address}`);
+                    console.log("You can view the message chain on the tangle", `${explorer}/mam/${initialRoot}`);
+                    console.log("or just for this message at", `${explorer}/mam/${message.address}`);
                 } else {
-                    console.log("You can view the transactions for this this message at", `https://thetangle.org/address/${message.address}`);
+                    console.log("You can view the transactions for this this message at", `${explorer}/address/${message.address}`);
                 }
                 fetchMessages(message.root);
+            })
+            .catch((err) => {
+                console.error("There was an error publishing the message", err)
             });
     });
