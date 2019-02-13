7ed46d980682a54850c4a97796f405033685c48b
Martyn Janes <martyn@obany.com>
2019-02-13 10:33:30 +0300

    Refresh to new libs

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+require('regenerator-runtime/runtime');
+const { logOutput, logError, logLink, clearOutput } = require("./output.js");
 const { asciiToTrytes, trytesToAscii } = require("@iota/converter");
 const Mam = require('../lib/mam.client.js');
 
@@ -7,11 +9,12 @@ const attachMwm = 14; // Use 9 for devnet
 const explorer = "https://thetangle.org" // use https://devnet.thetangle.org for devnet;
 
 // Initialise Mam, if you fork the sandbox you should change the seed
-console.log("Initializing Mam");
+clearOutput("Initializing Mam");
+logOutput("Initializing Mam");
 // Create a 81 trytes seed if you want to show previous messages in your mam stream
 const seed = undefined;
 if (!seed) {
-    console.error("As you have not provided an 81 tryte seed you will never get more than one message in your mam stream");
+    logError("As you have not populated an 81 tryte seed you will never get more than one message in your mam stream");
 }
 let mamState = Mam.init(provider, seed);
 const initialRoot = Mam.getRoot(mamState);
@@ -19,65 +22,79 @@ const initialRoot = Mam.getRoot(mamState);
 // Set the channel mode
 const channelMode = "public" // "private" "restricted"
 const retrictedSideKeyTrytes = channelMode === "restricted" ? "THIS9IS9A9RESTRICTED9KEY" : undefined;
-console.log("Channel Mode", channelMode);
+logOutput(`Channel Mode: ${channelMode}`);
 mamState = Mam.changeMode(mamState, channelMode, retrictedSideKeyTrytes);
 
 // Publish a new message
-function publishMessage(mamState, trytesMessage) {
-    console.log("Publishing Message", trytesMessage);
+async function publishMessage(mamState, trytesMessage) {
+    logOutput(`Publishing Message: ${trytesMessage}`);
 
     // Create MAM Payload
     const message = Mam.create(mamState, trytesMessage);
 
-    console.log("Root", message.root)
-    console.log("Address", message.address)
+    logOutput(`Root: ${message.root}`);
+    logOutput(`Address: ${message.address}`);
 
     // Attach the payload
-    console.log("Attaching payload, please wait...")
-    return Mam.attach(message.payload, message.address, attachDepth, attachMwm)
-        .then(() => message)
-        .catch((err) => {
-            console.error("There was an error attaching the message", err)
-        });
+    logOutput("Attaching payload, please wait...");
+
+    try {
+        await Mam.attach(message.payload, message.address, attachDepth, attachMwm);
+        return message;
+    } catch (err) {
+        logError("There was an error attaching the message", err);
+    }
 }
 
 // Fetch message beginning at the specific root.
-function fetchMessages(messageRoot) {
-    console.log("Fetching Messages from Root", messageRoot);
+async function fetchMessages(messageRoot) {
+    logOutput(`Fetching Messages from Root: ${messageRoot}`);
 
-    return Mam.fetch(messageRoot, channelMode, retrictedSideKeyTrytes)
-        .then(response => {
-            if (response) {
-                response.messages.forEach(messageTrytes => {
-                    console.log("Fetched Message", trytesToAscii(messageTrytes));
-                });
-                console.log("Next Root", response.nextRoot);
-                return response;
-            } else {
-                console.error("Unable to fetch messages.")
-            }
-        });
+    try {
+        const response = await Mam.fetch(messageRoot, channelMode, retrictedSideKeyTrytes);
+
+        if (response) {
+            response.messages.forEach(messageTrytes => {
+                logOutput(`Fetched Message: ${trytesToAscii(messageTrytes)}`);
+            });
+            logOutput(`Next Root: ${response.nextRoot}`);
+        }
+        return response;
+    } catch (err) {
+        logError("There was an error fetching messages", err);
+    }
 }
+
+
 
 // Get the current messages from the root to find the start count
 // to publish the new message. You could store the start count
 // somewhere else and load it here to avoid having to walk
 // the tree just to publish a message.
-fetchMessages(Mam.getRoot(mamState))
-    .then((messageResponse) => {
+(async function () {
+    try {
+        const messageResponse = await fetchMessages(Mam.getRoot(mamState))
+
         if (messageResponse) {
             mamState.channel.start = messageResponse.messages.length;
 
-            publishMessage(mamState, asciiToTrytes(`This is my message ${messageResponse.messages.length + 1}`))
-                .then((message) => {
-                    console.log("Message Published");
-                    if (channelMode === "public") {
-                        console.log("You can view the message chain on the tangle", `${explorer}/mam/${initialRoot}`);
-                        console.log("or just for this message at", `${explorer}/mam/${message.address}`);
-                    } else {
-                        console.log("You can view the transactions for this this message at", `${explorer}/address/${message.address}`);
-                    }
-                    fetchMessages(message.root);
-                });
+            const message = await publishMessage(mamState, asciiToTrytes(`This is my message ${messageResponse.messages.length + 1}`));
+            console.log(message);
+            logOutput("Message Published");
+            if (channelMode === "public") {
+                logOutput(`You can view the message chain on the tangle:`);
+                logLink(`${explorer}/mam/${initialRoot}`);
+                logOutput(`or just for this message at:`);
+                logLink(`${explorer}/mam/${message.address}`);
+            } else {
+                logOutput(`You can view the transactions for this this message at:`);
+                logLink(`${explorer}/address/${message.address}`);
+            }
+            await fetchMessages(message.root);
         }
-    });
+    } catch (err) {
+        logError("Error fetching messages", err);
+    }
+    logOutput("Finished.");
+})();
+
